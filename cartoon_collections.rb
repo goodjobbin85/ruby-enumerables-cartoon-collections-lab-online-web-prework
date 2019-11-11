8a0798a971150c20ba0da8e02ef63665f77c719e
thomas dubiel <td1729@hotmail.com>
2019-11-11 23:31:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(list) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   list.each do |thing| 
-    if cheese_types.include?(thing) 
+    if cheese_types.include?("#{thing}") 
       return thing
     else 
       return nil 
